100ecd9afab36e6cf7455d02a75875c15b6003ff
table-harmony <lironkaner2007@gmail.com>
2024-05-03 11:31:05 +0300

    shadcn-ui

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "gamingharmony",
+  url: "https://gamingharmony.vercel.app",
+  author: "liron kaner",
+  description: "gaming hub",
+  links: {
+    github: "https://github.com/table-harmony/gamingharmony",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
