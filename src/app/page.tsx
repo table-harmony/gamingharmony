81fe8913c0d76a0c1eb7664d46be2a24f999ecc3
table-harmony <lironkaner2007@gmail.com>
2024-05-03 11:52:52 +0300

    games

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import Link from "next/link";
+
 export default function Home() {
-  return <>gaming</>;
+  const games = [
+    "https://editor.p5js.org/liron1111/full/zVcvttfav",
+    "https://editor.p5js.org/liron1111/full/mEdQGp3iq",
+    "https://editor.p5js.org/liron1111/full/gUsW_PQRt",
+    "https://editor.p5js.org/liron1111/full/3Nc0OLyPn",
+    "https://editor.p5js.org/liron1111/full/CJNDb37JQ",
+    "https://editor.p5js.org/liron1111/full/xyI99_kLl",
+    "https://editor.p5js.org/liron1111/full/xYiNjZ21I",
+    "https://editor.p5js.org/liron1111/full/bBjnsIQDq",
+    "https://editor.p5js.org/liron1111/full/RsJNbs5gO",
+  ];
+
+  return (
+    <div className="flex flex-col gap-2">
+      {games.map((game) => (
+        <Link href={game} key={game} target="_blank" rel="noreferrer">
+          {game}
+        </Link>
+      ))}
+      <ModeToggle />
+    </div>
+  );
 }
